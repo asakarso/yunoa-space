fa9330c4abe27bee3fad9593e5ed9575fadd021b
Jordan Posh Tambunan <tambunan.2308561091@student.unud.ac.id>
2025-05-31 20:54:02 +0800

    tambah rute konseling payment web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\CounselingController;
+
 use Illuminate\Support\Facades\Route;
+
+Route::get('/counseling/payment/{doctor_id}', [CounselingController::class, 'showPayment'])->name('counseling.payment');
 
 Route::get('/', function () {
     return view('welcome');
