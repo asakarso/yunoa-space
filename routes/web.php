4f8003eb80402be8c2f1f5b40af8c70aa21d2627
Jordan Posh Tambunan <tambunan.2308561091@student.unud.ac.id>
2025-06-04 15:00:32 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\CounselingController;
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/counseling/payment/{doctor_id}', [CounselingController::class, 'showPayment'])->name('counseling.payment');
+    Route::post('/counseling/payment/{doctor_id}', [CounselingController::class, 'processPayment'])->name('counseling.processPayment');
+});
 
 use Illuminate\Support\Facades\Route;
 
