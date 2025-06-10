8a4e6aefee445c9a7fad87c3b5fd9fb2e9f07404
I Putu Agus Wahyu Wirakusuma Putra <putuaguswahyuwp@gmail.com>
2025-06-10 18:11:38 +0800

    Menambahkan register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\AssessmentController;
 use App\Http\Controllers\CounselingController;
@@ -13,6 +14,10 @@ Route::middleware('guest')->group(function () {
     // Login routes
     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [LoginController::class, 'login']);
+
+    // Register routes
+    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
 });
 
 Route::middleware(['auth', 'role:Admin'])->group(function () {
