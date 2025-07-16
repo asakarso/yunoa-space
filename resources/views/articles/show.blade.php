a6654a10e3d8177d5eda0a7437027f6e2e1131c1
denasatwika <158028249+denasatwika@users.noreply.github.com>
2025-07-16 15:30:24 +0800

    Update show.blade.php pada link article

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -75,7 +75,7 @@
                         <div class="d-flex flex-column gap-3">
                             @forelse ($relatedArticles as $related)
                                 <a href="{{ route('articles.show', $related->id_review) }}" class="side-article-item">
-                                    <img src="{{ asset('storage/' . $related->gambar_cover) }}" alt="{{ $related->judul_artikel }}">
+                                    <img src="{{ asset('storage/article_covers/' . $related->gambar_cover) }}" alt="{{ $related->judul_artikel }}">
                                     <div>
                                         <p class="fw-bold mb-1">{{ $related->judul_artikel }}</p>
                                         {{-- <small class="text-muted">Kesehatan • 5 menit</small> --}}
